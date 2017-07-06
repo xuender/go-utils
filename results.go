9b64302e7c433b5b0e4cc7029fdf88474a55bee5
徐晨阳 <xuender@gmail.com>
2017-07-06 19:39:57 +0800

    fix #35

--- results.go
+++ results.go
@@ -5,16 +5,16 @@ import (
 	"sync"
 )
 
-type result struct {
-	data  interface{}
-	point interface{}
+type Result struct {
+	Data  interface{}
+	Point interface{}
 }
 
 // Results 最优结果集.
 type Results struct {
 	Len   int
-	datas []result
-	size  int
+	Data  []Result
+	Size  int
 	less  func(i, j interface{}) bool
 	mutex sync.Mutex
 }
@@ -23,14 +23,14 @@ type Results struct {
 func (r *Results) Add(data, point interface{}) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	r.datas[r.Len] = result{
-		data:  data,
-		point: point,
+	r.Data[r.Len] = Result{
+		Data:  data,
+		Point: point,
 	}
 	r.Len++
-	sort.Slice(r.datas[:r.Len], func(i, j int) bool { return r.less(r.datas[i].point, r.datas[j].point) })
-	if r.Len > r.size {
-		r.Len = r.size
+	sort.Slice(r.Data[:r.Len], func(i, j int) bool { return r.less(r.Data[i].Point, r.Data[j].Point) })
+	if r.Len > r.Size {
+		r.Len = r.Size
 	}
 }
 
@@ -43,30 +43,30 @@ func (r *Results) AddResults(results *Results) {
 
 // Get 获取数据.
 func (r *Results) Get(i int) (interface{}, interface{}) {
-	return r.datas[i].data, r.datas[i].point
+	return r.Data[i].Data, r.Data[i].Point
 }
 
 // GetData 数据.
 func (r *Results) GetData(i int) interface{} {
-	return r.datas[i].data
+	return r.Data[i].Data
 }
 
 // GetPoint 得分.
 func (r *Results) GetPoint(i int) interface{} {
-	return r.datas[i].point
+	return r.Data[i].Point
 }
 
 // GetSize 尺寸.
 func (r *Results) GetSize() int {
-	return r.size
+	return r.Size
 }
 
 // NewResults 新建结果集.
 func NewResults(size int, less func(i, j interface{}) bool) *Results {
 	return &Results{
 		Len:   0,
-		datas: make([]result, size+1),
-		size:  size,
+		Data: make([]Result, size+1),
+		Size:  size,
 		less:  less,
 	}
 }
