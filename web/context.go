4d8d798b7ae531149ff9ab93d320ab68e3f79c3f
ender <xuender@gmail.com>
2018-08-09 14:41:02 +0800

    web Get

--- web/context.go
+++ web/context.go
@@ -3,6 +3,7 @@ package web
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 )
 
@@ -10,4 +11,30 @@ type Context struct {
 	Writer  http.ResponseWriter
 	Request *http.Request
 	Render  *render.Render
+	values  map[string]string
+}
+
+func (c *Context) Text(status int, v string) error {
+	return c.Render.Text(c.Writer, status, v)
+}
+func (c *Context) HTML(status int, name string, binding interface{}, htmlOpt ...render.HTMLOptions) error {
+	return c.Render.HTML(c.Writer, status, name, binding, htmlOpt...)
+}
+func (c *Context) Data(status int, v []byte) error {
+	return c.Render.Data(c.Writer, status, v)
+}
+func (c *Context) JSON(status int, v interface{}) error {
+	return c.Render.JSON(c.Writer, status, v)
+}
+func (c *Context) JSONP(status int, callback string, v interface{}) error {
+	return c.Render.JSONP(c.Writer, status, callback, v)
+}
+func (c *Context) XML(status int, v interface{}) error {
+	return c.Render.XML(c.Writer, status, v)
+}
+func (c *Context) Get(key string) string {
+	if c.values == nil {
+		c.values = mux.Vars(c.Request)
+	}
+	return c.values[key]
 }
--- web/web.go
+++ web/web.go
@@ -21,13 +21,21 @@ func Classic() *Web {
 		Negroni: negroni.Classic(),
 	}
 }
-
-func (w *Web) Get(path string, f func(c *Context)) *mux.Route {
+func (w *Web) Handle(path string, f func(c *Context)) *mux.Route {
 	return w.Router.HandleFunc(path, func(writer http.ResponseWriter, req *http.Request) {
 		f(&Context{
 			Writer:  writer,
 			Request: req,
 			Render:  w.Render,
 		})
-	}).Methods("GET")
+	})
+}
+func (w *Web) Get(path string, f func(c *Context)) *mux.Route {
+	return w.Handle(path, f).Methods("GET")
+}
+func (w *Web) Post(path string, f func(c *Context)) *mux.Route {
+	return w.Handle(path, f).Methods("POST")
+}
+func (w *Web) Put(path string, f func(c *Context)) *mux.Route {
+	return w.Handle(path, f).Methods("PUT")
 }
