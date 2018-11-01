282b6cd309c396cf5aa18dc75211bfb27a396435
ender <xuender@gmail.com>
2018-11-01 09:50:55 +0800

    feat: add IDS

---
+++ ids.go
@@ -0,0 +1,59 @@
+package utils
+
+// IDS slice ID
+type IDS []ID
+
+// Add ids
+func (s *IDS) Add(ids ...ID) []ID {
+	*s = append(*s, ids...)
+	return *s
+}
+
+// IndexOf returns the index of the matched id, else -1.
+func (s *IDS) IndexOf(id ID) int {
+	for f, v := range *s {
+		if id == v {
+			return f
+		}
+	}
+	return -1
+}
+
+// Contains returns true if id is found, else false.
+func (s *IDS) Contains(id ID) bool {
+	for _, v := range *s {
+		if id == v {
+			return true
+		}
+	}
+	return false
+}
+
+// Delete removes all occurrences of ids in the slice.
+func (s *IDS) Delete(ids ...ID) []ID {
+	for _, v := range ids {
+		for {
+			f := s.IndexOf(v)
+			if f < 0 {
+				break
+			}
+			*s = append((*s)[:f], (*s)[f+1:]...)
+		}
+	}
+	return *s
+}
+
+// Intersect returns the slice of intersecting values.
+// Union see Add
+// Except see Delete
+func (s *IDS) Intersect(ids ...ID) []ID {
+	b := IDS(ids)
+	del := IDS{}
+	for _, i := range *s {
+		if !b.Contains(i) {
+			del.Add(i)
+		}
+	}
+	s.Delete(del...)
+	return *s
+}
---
+++ ids_test.go
@@ -0,0 +1,114 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestIDS(t *testing.T) {
+	Convey("NewIDS", t, func() {
+		ids := IDS{}
+		id := NewID('A')
+		ids.Add(id)
+		Convey("len", func() {
+			So(len(ids), ShouldEqual, 1)
+		})
+		Convey("Add", func() {
+			b := IDS{}
+			b.Add(NewID('A'), NewID('A'), NewID('A'))
+			ids.Add(b...)
+			So(len(ids), ShouldEqual, 4)
+		})
+	})
+}
+
+func ExampleIDS() {
+	ids := IDS{}
+	ids.Add(NewID('A'))
+
+	fmt.Println(len(ids))
+
+	// Output:
+	// 1
+}
+
+func ExampleIDS_Add() {
+	ids := IDS{}
+	ids.Add(NewID('A'), NewID('A'))
+
+	fmt.Println(len(ids))
+
+	// Output:
+	// 2
+}
+
+func ExampleIDS_IndexOf() {
+	ids := IDS{}
+	id := NewID('K')
+	ids.Add(NewID('A'), NewID('A'))
+	ids.Add(id)
+
+	fmt.Println(ids.IndexOf(id))
+
+	// Output:
+	// 2
+}
+
+func ExampleIDS_Contains() {
+	ids := IDS{}
+	id := NewID('K')
+	ids.Add(NewID('A'), NewID('A'))
+	ids.Add(id)
+
+	fmt.Println(ids.Contains(id))
+	fmt.Println(ids.Contains(NewID('A')))
+
+	// Output:
+	// true
+	// false
+}
+func ExampleIDS_Delete() {
+	ids := IDS{}
+	id := NewID('K')
+	ids.Add(NewID('A'), NewID('A'))
+	ids.Add(id)
+
+	fmt.Println(len(ids))
+	fmt.Println(ids.Contains(id))
+	fmt.Println(len(ids.Delete(id)))
+	fmt.Println(ids.Contains(id))
+
+	// Output:
+	// 3
+	// true
+	// 2
+	// false
+}
+
+func ExampleIDS_Intersect() {
+	ids := IDS{}
+	id := NewID('K')
+	ids.Add(NewID('A'), NewID('A'))
+	ids.Add(id)
+	ids.Intersect(id, NewID('A'))
+
+	fmt.Println(len(ids))
+
+	// Output:
+	// 1
+}
+func BenchmarkContains(b *testing.B) {
+	b.StopTimer()
+	ids := IDS{}
+	id := NewID('A')
+	for i := 0; i < 1000; i++ {
+		ids.Add(NewID('A'))
+	}
+	ids.Add(id)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		ids.Contains(id)
+	}
+}
